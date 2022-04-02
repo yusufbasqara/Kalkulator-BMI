3ca14debc9a7c7df6774304b0cc0bdb171bf0dc9
yusufbasqara <yusufbasqara@student.telkomuniversity.ac.id>
2022-04-02 18:01:58 +0700

    intial commit

---
+++ app/src/main/java/com/d3if3071/assesment1_kalkulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.d3if3071.assesment1_kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
