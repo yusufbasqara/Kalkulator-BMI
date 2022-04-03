fda369beee40dd15c2924c4c889c2dba705d5a6b
yusufbasqara <yusufbasqara@student.telkomuniversity.ac.id>
2022-04-04 00:08:43 +0700

    memperbaiki error toast dan hitungan yang berhenti

--- app/src/main/java/com/d3if3071/assesment1_kalkulator/MainActivity.kt
+++ app/src/main/java/com/d3if3071/assesment1_kalkulator/MainActivity.kt
@@ -30,31 +30,38 @@ class MainActivity : AppCompatActivity() {
 
 
         val lebar = binding.editTextNumber2.text.toString()
-        if (TextUtils.isEmpty(panjang)) {
+        if (TextUtils.isEmpty(lebar)) {
             Toast.makeText(this, R.string.lebar_invalid, Toast.LENGTH_SHORT).show()
         }
 
         val tinggi = binding.editTextNumber3.text.toString()
-        if (TextUtils.isEmpty(panjang)) {
-            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_SHORT).show()
-        }
+
 
         val selectedId = binding.radioGroup.checkedRadioButtonId
+
         if (selectedId == 2131230955 || selectedId == 2131230807) {
-            val hasilBangunRuang = (panjang.toDouble() * lebar.toDouble() * tinggi.toDouble())
-            binding.hasilLuas.text = hasilBangunRuang.toString()
-            Toast.makeText(this, "3 parameter", Toast.LENGTH_LONG).show()
-            return
+            if (TextUtils.isEmpty(tinggi)) {
+                Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_SHORT).show()
+            }
+            if (!TextUtils.isEmpty(panjang) && !TextUtils.isEmpty(lebar) && !TextUtils.isEmpty(
+                    tinggi
+                )
+            ) {
+                val hasilBangunRuang =
+                    (panjang.toDouble() * lebar.toDouble() * tinggi.toDouble())
+                binding.hasilLuas.text = hasilBangunRuang.toString()
+                Toast.makeText(this, "3 parameter", Toast.LENGTH_LONG).show()
+                return
+            }
         } else {
             Toast.makeText(this, "2 parameter", Toast.LENGTH_LONG).show()
             val hasilBangunDatar = panjang.toDouble() * lebar.toDouble()
             binding.hasilLuas.text = hasilBangunDatar.toString()
             return
+
         }
 
         Log.d("check radio", "${selectedId}")
-
-
 
 
     }
